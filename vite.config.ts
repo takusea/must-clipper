afdceb3d962eaa73d8058ee39fbe4d4405c2d362
Takusea <takusea826@gmail.com>
2024-10-20 03:06:17 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: process.env.GITHUB_PAGES ? "multi-clipper" : "./",
 	plugins: [react()],
 });

085e5963dbd7ea94f8f5a3f2efb3c5f0517263f8
Takusea <takusea826@gmail.com>
2024-10-20 23:45:01 +0900

    名称を変更

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: process.env.GITHUB_PAGES ? "multi-clipper" : "./",
+	base: process.env.GITHUB_PAGES ? "must-clipper" : "./",
 	plugins: [react()],
 });

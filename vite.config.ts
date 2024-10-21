87bb4dd4c326cdf6cc414f045c126141529de81f
Takusea <takusea826@gmail.com>
2024-10-21 21:36:27 +0900

    VitePWAの追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,39 @@
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: process.env.GITHUB_PAGES ? "must-clipper" : "./",
-	plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: "autoUpdate",
+			includeAssets: ["icon.png"],
+			injectRegister: "auto",
+			manifest: {
+				name: "ますとくりっぱー",
+				short_name: "ますとくりっぱー",
+				description: "YouTube・Twitch用配信まとめツール",
+				theme_color: "#5eead4",
+				icons: [
+					{
+						src: "icon.png",
+						sizes: "128x128",
+						type: "image/png",
+					},
+				],
+				share_target: {
+					action: "/share-target/",
+					method: "GET",
+					enctype: "application/x-www-form-urlencoded",
+					params: {
+						title: "title",
+						text: "text",
+						url: "url",
+					},
+				},
+			},
+		}),
+	],
 });

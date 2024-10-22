8fd243d36fd46c5baf904ff5505b09e1070157e7
Takusea <takusea826@gmail.com>
2024-10-23 03:18:58 +0900

    TimeFieldのステップ数を修正

--- src/components/TimeField.tsx
+++ src/components/TimeField.tsx
@@ -13,7 +13,7 @@ export function TimeField(props: Props) {
 		<input
 			className="border border-black/20 hover:border-black/30 h-10 rounded flex-grow px-2 shadow-inner"
 			type="time"
-			step={1000}
+			step={1}
 			placeholder={props.placeholder}
 			id={props.id}
 			value={props.value}
